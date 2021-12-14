4eabc77a779c1bdccc6e2489363ff2e7ddcbf2ec
David-Chazoule <d.chazoule@gmail.com>
2021-12-14 15:22:25 +0100

    filter composant

---
+++ src/components/filter/Choicebox.jsx
@@ -0,0 +1,17 @@
+
+import '../../CSS/filter/choicebox.css';
+
+function Choicebox () {
+    return(
+        <div className="choicebox">
+         <img src="" alt="informatique" />
+         <img src="" alt="téléphones" />
+         <img src="" alt="jeux vidéo" />
+         <img src="" alt="casque" />
+         <img src="" alt="tv" />
+         <img src="" alt="vr" />
+        </div>
+    )
+}
+
+export default Choicebox;
---
+++ src/components/filter/FilterContainer.jsx
@@ -0,0 +1,19 @@
+
+import '../../CSS/filter/filterContainer.css';
+
+
+
+function FiltrerContainer () {
+    return (
+        < div className="filter_container">
+
+            
+        
+        
+        
+        </div>
+    )
+}
+
+
+export default FiltrerContainer;
